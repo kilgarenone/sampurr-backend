beabf8a3008dafd9eca19e9981793ee34f440c28
Kheoh Yee Wei <oldjoy@protonmail.com>
2021-09-27 09:37:43 +0800

    stuff

--- app.js
+++ app.js
@@ -1,86 +1,91 @@
 const express = require("express");
 const cors = require("cors");
 const createError = require("http-errors");
-const fs = require('fs');
-const path = require('path')
-const {
-    spawn
-} = require('child_process');
+const fs = require("fs");
+const path = require("path");
+const execa = require("execa");
 
 const app = express();
 
 const isProduction = app.get("env") === "production";
 
 if (isProduction) {
-    app.set("trust proxy", 1);
+  app.set("trust proxy", 1);
 }
 
 app.disable("x-powered-by");
 
 app.set("port", process.env.PORT || 4000);
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("tmp"));
 
 // url of your client
 const ALLOWED_ORIGINS = [
-    "http://localhost:8008" /* "https://example-prod-app.com */ ,
+  "http://localhost:8008" /* "https://example-prod-app.com */,
 ];
 
 app.use(
-    cors({
-        credentials: true, // include Access-Control-Allow-Credentials: true. remember set xhr.withCredentials = true;
-        origin(origin, callback) {
-            // allow requests with no origin
-            // (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-            if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
-                const msg =
-                    "The CORS policy for this site does not " +
-                    "allow access from the specified Origin.";
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
-    })
+  cors({
+    credentials: true, // include Access-Control-Allow-Credentials: true. remember set xhr.withCredentials = true;
+    origin(origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
 );
 
-// could be one of your API endpoints for your client
-app.get("/haha", (req, res) => {
-    var source = path.join(__dirname, 'small.mp3');
-    var target = path.join(__dirname, 'small.json');
+app.get("/haha", async (req, res) => {
+  await execa("youtube-dl", [
+    "https://www.youtube.com/watch?v=bamxPYj0O9M",
+    "--prefer-ffmpeg",
+    "--extract-audio",
+    "--audio-format",
+    "mp3",
+    "--output",
+    path.join(__dirname, "tmp", "%(id)s.%(ext)s"),
+  ]);
 
-    var audiowave = spawn(
-        'audiowaveform', ['-i', source, '-o', target]
-    );
+  const target = path.join(__dirname, "tmp", "bamxPYj0O9M.json");
 
-    audiowave.on('close', function(code, signal) {
-        res.header("Content-Type", 'application/json');
-        res.sendFile(target);
-    });
+  await execa("audiowaveform", ["-i", "tmp/bamxPYj0O9M.mp3", "-o", target]);
 
+  res.header("Content-Type", "application/json");
+  res.sendFile(target);
 });
 
 // central custom error handler
 // NOTE: DON"T REMOVE THE 'next'!!!!!
-app.use(function(err, req, res, next) {
-    const error = createError(500, "Something went wrong. Alerted developer");
+app.use(function (err, req, res, next) {
+  const error = createError(500, "Something went wrong. Alerted developer");
 
-    res.status(error.status).json(error);
+  res.status(error.status).json(error);
 });
 
 process.on("unhandledRejection", (reason, p) => {
-    // Error not caught in promises(ie. forgot the 'catch' block) will get swallowed and disappear.
-    // I just caught an unhandled promise rejection,
-    // since we already have fallback handler for unhandled errors (see below),
-    // let throw and let him handle that
-    throw reason;
+  // Error not caught in promises(ie. forgot the 'catch' block) will get swallowed and disappear.
+  // I just caught an unhandled promise rejection,
+  // since we already have fallback handler for unhandled errors (see below),
+  // let throw and let him handle that
+  throw reason;
 });
 
 // mainly to catch those from third-party lib. for own code, catch it in try/catch
-process.on("uncaughtException", function(err) {
-    process.exit(1);
+process.on("uncaughtException", function (err) {
+  process.exit(1);
 });
 
 module.exports = app;
