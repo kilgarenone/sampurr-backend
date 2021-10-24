d4dc5c6a5838bdfcf525ad18f0228d660efa4d3d
yeewei <kwei88@gmail.com>
2021-10-24 21:31:53 +0800

    s

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,9 +3,12 @@ module.exports = {
     {
       name: "sampurr",
       script: "./server.js",
+      instances: 0,
       exec_mode: "cluster",
       max_memory_restart: "170M",
       merge_logs: true, // all instances of a clustered process logs into the same file when in cluster mode
+      time: true, // prefix timestamp to log
+      log_date_format: "YYYY-MM-DD HH:mm Z",
     },
   ],
 };
